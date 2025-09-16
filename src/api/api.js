5140cf2b59ba7770e671143c5f9a27ba19e60157
Sushil <sushil.codepro@gmail.com>
2025-09-16 18:56:22 +0530

    inprogess feed api

--- src/api/api.js
+++ src/api/api.js
@@ -3,3 +3,4 @@ import apiClient from "./httpClient";
 export const loginAPI= (data)=>apiClient.post('/user/auth/signin',data,{withCredentials:true});
 export const fetchProfile =()=>apiClient.get('http://localhost:3000/user/profile/view/',{withCredentials:true});
 export const logoutAPI =()=>apiClient.get('http://localhost:3000/user/auth/logout/',{withCredentials:true});
+export const feedAPI =()=>apiClient.get('http://localhost:3000/user/feed/',{withCredentials:true});
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { feedAPI } from '../api/api'
+import { useQuery } from '@tanstack/react-query'
 
 const Feed = () => {
+
+    const { data, isLoading, isError } = useQuery({
+      queryKey: ["feed"],
+      queryFn: feedAPI,
+      onSuccess: (data) => {
+        console.log("Feed fetched:", data);
+      },
+      onError: (error) => {
+        console.error("Failed to fetch Feed:", error);
+      },
+    });
   return (
     <div>Feed</div>
   )
