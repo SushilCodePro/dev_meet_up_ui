94dcee801e009a56d0a990b709e5a6343a9a28cd
Sushil Gupta <sushil.codepro@gmail.com>
2025-11-01 10:41:44 +0530

    Remove API time out

--- src/api/httpClient.js
+++ src/api/httpClient.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASEURL,
-  timeout: 10000,
+  // timeout: 10000,
   withCredentials:true,
   headers: { 'Content-Type': 'application/json' },
 });
